f7865e6ee8d74352b0d8a6e4147680d6e2ff1f46
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-25 13:41:14 +0100

    Boton Sumar

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [ firstNumber, setFirstNumber ] = useState(null);
-
-  function changeFirstNumberHandler (event) {
-    setFirstNumber(event.target.value);
-  }
-
-  useEffect(
-    ()=>{
-      console.log("firstNumber state:", firstNumber);
+    const [ firstNumber, setFirstNumber ] = useState("");
+    const [ secondNumber, setSecondNumber ] = useState("");
+  //`p`asociado a un nuevo hook state `result`
+    const [result,setResult]=useState(null);
+    
+    function changeFirstNumberHandler (event) {
+      setFirstNumber(parseFloat(event.target.value));
     }
-  )
+    function changeSecondNumberHandler (event) {
+      setSecondNumber(event.target.value);
+    }
 
+
+
+      console.log("firstNumber state:", firstNumber);
+      console.log("secondNumber state:", secondNumber);
+      console.log("result state:", result);
+
+      //FUNCION SUMAR//
+      function addHandler() {
+        const result = firstNumber + secondNumber;
+        setResult(result);
+        setFirstNumber(result);
+      }
   return (
     <>
       <h1>Calculadora</h1>
       <input type="text" value={firstNumber} onChange={changeFirstNumberHandler}/>
+      <input type="text" value={secondNumber} onChange={changeSecondNumberHandler}/>
+      <p>{result}</p>
+      <button onClick={addHandler}>sumar</button>
     </>
   );
-}
+  }
 
 export default App;
