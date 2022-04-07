5fc6e1a8a1335ee4287da1180ce2f2b666992503
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-07 13:47:17 +0200

    ejerciciso

--- src/App.js
+++ src/App.js
@@ -1,40 +1,109 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
-    const [ firstNumber, setFirstNumber ] = useState("");
-    const [ secondNumber, setSecondNumber ] = useState("");
-  //`p`asociado a un nuevo hook state `result`
-    const [result,setResult]=useState(null);
-    
-    function changeFirstNumberHandler (event) {
-      setFirstNumber(parseFloat(event.target.value));
-    }
-    function changeSecondNumberHandler (event) {
-      setSecondNumber(event.target.value);
-    }
+  //Siempre que declaro un state se almacenan estos 2 nombres: uno para almacenar y otro para cambiar
+  const [firstNumber, setFirstNumber] = useState("");
+  const [secondNumber, setSecondNumber] = useState("");
+  const [result, setResult] = useState(null);
+  const memory = useRef(0);
 
+  const [resultsHistory, setResultsHistory] = useState([]);
 
+  function changeFirstNumberHandler(event) {
+    setFirstNumber(parseFloat(event.target.value));
+  }
 
+  function changeSecondNumberHandler(event) {
+    setSecondNumber(parseFloat(event.target.value));
+  }
+
+  useEffect(
+    () => {
       console.log("firstNumber state:", firstNumber);
       console.log("secondNumber state:", secondNumber);
-      console.log("result state:", result);
+    }
+  )
 
-      //FUNCION SUMAR//
-      function addHandler() {
-        const result = firstNumber + secondNumber;
-        setResult(result);
-        setFirstNumber(result);
-      }
+  
+  useEffect(
+    ()=>{
+      //Dispersión
+        //setResultsHistory([...resultsHistory,result]);
+
+        //La dispersión es este proceso:
+        const resultsHistory2=Array.from(resultsHistory);
+        resultsHistory2.push(result);
+        setResultsHistory(resultsHistory2);
+        
+    },
+    [result]
+)
+
+
+  function addHandler() {
+    const result = firstNumber + secondNumber;
+    setResult(result);
+    setFirstNumber(result);
+  }
+
+  function restHandler() {
+    const result = firstNumber - secondNumber;
+    setResult(result);
+    setFirstNumber(result);
+  }
+
+  function multiplyHandler() {
+    const result = firstNumber * secondNumber;
+    setResult(result);
+    setFirstNumber(result);
+  }
+
+  function divideHandler() {
+    const result = firstNumber / secondNumber;
+    setResult(result);
+    setFirstNumber(result);
+  }
+
+  function deleteHandler() {
+    setFirstNumber(0);
+    setSecondNumber(0);
+    setResult(0);
+  }
+
+  function deleteHistory(){
+    setResultsHistory([""]);
+  }
+
+  function MHandler() {
+    memory.current = result;
+    console.log(memory);
+  }
+
+  function MRHandler() {
+    setFirstNumber(memory.current);
+  }
+
   return (
     <>
       <h1>Calculadora</h1>
-      <input type="text" value={firstNumber} onChange={changeFirstNumberHandler}/>
-      <input type="text" value={secondNumber} onChange={changeSecondNumberHandler}/>
+      <input type="text" value={firstNumber} onChange={changeFirstNumberHandler} /><br />
+      <input type="text" value={secondNumber} onChange={changeSecondNumberHandler} />
+
       <p>{result}</p>
       <button onClick={addHandler}>sumar</button>
+      <button onClick={restHandler}>restar</button>
+      <button onClick={multiplyHandler}>multiplicar</button>
+      <button onClick={divideHandler}>dividir</button>
+      <button onClick={deleteHandler}>C</button>
+      <button onClick={MHandler}>M+</button>
+      <button onClick={MRHandler}>MR</button>
+      <button onClick={deleteHistory}>Borrar historial</button>
+      <h2>History</h2>
+      <History results={resultsHistory}/>
     </>
   );
-  }
+}
 
 export default App;
